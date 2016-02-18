d85e55b1b6eaa7d2bfcdb8fdcc4ee5c848dfd1f7
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2016-02-17 20:51:16 -0600

    passes test

--- bob.rb
+++ bob.rb
@@ -2,12 +2,12 @@ class Bob
 
 
   def hey(remark)
-    if (remark == remark.reverse)
+    if (remark == remark.upcase) && (remark.downcase != remark.upcase)
+      'Whoa, chill out!'
+    elsif (remark == remark.reverse)
       'Fine. Be that way!'
     elsif remark.end_with?("?")
       'Sure.'
-    elsif (remark == remark.upcase) && (remark != remark.reverse)
-      'Whoa, chill out!'
     else
       'Whatever.'
     end
